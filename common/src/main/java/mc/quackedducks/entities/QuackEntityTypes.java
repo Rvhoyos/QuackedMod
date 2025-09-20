6d585d8d694a676ff7b06261269ae4ce06809be8
rvhoyos <rvhoyos@gmail.com>
2025-09-20 19:57:20 -0400

    Added eggs breaking with particle effects in common code

--- common/src/main/java/mc/quackedducks/entities/QuackEntityTypes.java
+++ common/src/main/java/mc/quackedducks/entities/QuackEntityTypes.java
@@ -12,8 +12,6 @@ import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
-import net.minecraft.world.entity.EntityDimensions;
-import net.minecraft.world.entity.Pose;
 
 import net.minecraft.world.entity.Entity;
 
@@ -47,7 +45,7 @@ public class QuackEntityTypes {
             () -> EntityType.Builder.<mc.quackedducks.entities.projectile.DuckEggEntity>of(
                     mc.quackedducks.entities.projectile.DuckEggEntity::new, MobCategory.MISC)
                 .sized(0.25f, 0.50f)
-                .clientTrackingRange(4)
+                .clientTrackingRange(64)
                 .updateInterval(10)
                 .build(ResourceKey.create(Registries.ENTITY_TYPE,
                     ResourceLocation.fromNamespaceAndPath(QuackMod.MOD_ID, "duck_egg_projectile")))
